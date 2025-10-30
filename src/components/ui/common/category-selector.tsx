b98026808c0fb1af21408fa66c05578679f44d85
juliarcoelho <juliarochx@gmail.com>
2025-10-30 18:50:31 -0300

    feat: add category products page

--- src/components/ui/common/category-selector.tsx
+++ src/components/ui/common/category-selector.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
 import { categoryTable } from "@/db/schema";
 
@@ -15,7 +17,7 @@ const CategorySelector = ({ categories }: CategorySelectorProps) => {
             variant="ghost"
             className="rounded-full bg-white text-xs font-semibold"
           >
-            {category.name}
+            <Link href={`/category/${category.slug}`}>{category.name}</Link>
           </Button>
         ))}
       </div>
