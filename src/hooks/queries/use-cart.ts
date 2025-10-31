7c4241ea2826c67d44942017b20ee7a604a38ba6
juliarcoelho <juliarochx@gmail.com>
2025-10-31 11:09:16 -0300

    feat: ensure cart shipping address can be selected

--- src/hooks/queries/use-cart.ts
+++ src/hooks/queries/use-cart.ts
@@ -3,10 +3,12 @@ import { useQuery } from "@tanstack/react-query";
 import { getCart } from "@/actions/get-cart";
 
 export const getUseCartQueryKey = () => ["cart"] as const;
-
-export const useCart = () => {
+export const useCart = (params?: {
+  initialData?: Awaited<ReturnType<typeof getCart>>;
+}) => {
   return useQuery({
     queryKey: getUseCartQueryKey(),
     queryFn: () => getCart(),
+    initialData: params?.initialData,
   });
 };
